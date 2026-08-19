991344212de26e3832bfa3714b0e586b8ebe5026
dhruvkhanna <dhruvkhanna2163@gmail.com>
2026-08-19 13:31:54 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState , useContext } from 'react'
 import Login from './components/Auth/Login'
 import EmployeeDashboard from './components/Dashboard/EmployeeDashboard'
 import Header from './others/Header'
 import AdminDashboard from './components/Dashboard/AdminDashboard'
-import AuthContext from './context/AuthContext'
 import { getLocalStorage, setLocalStorage } from './utils/LocalStorage'
+import AuthProvider from './context/AuthProvider'
+import { AuthContext } from './context/AuthProvider'
 
 
 
@@ -15,32 +16,39 @@ const App = () => {
   //   getLocalStorage()
   // })
 
+
   const[user,setUser] = useState(null)
+  const authData = useContext(AuthContext)
+  console.log(authData.employees) 
 
   const handleLogin = (email,password) => {
-    if (email == " " && password == "123"){
-      console.log("this is admin")
+    if (email == "admin@me.com" && password == "123"){
+      setUser('admin')
+      console.log(user)
     }
-    else{
-      alert("Invalid credentials")
+    else if(email == 'user@me.com' && password ==  '123'){
+      setUser('employee')
+      console.log(user)
     
+    }
+    else {
+      alert('Invalid Credentials')
     }
 
   }
 
   
-  
+  const data = useContext(AuthContext)
   
   return (
-    <div>
-      {/* if user is not there then log in or if it is there , put it blank */}
-       {!user? <Login  handleLogin = {handleLogin} /> : ' '}
-      
-      {/* <EmployeeDashboard/> */}
-      {/* <AdminDashboard/>   */}
-      {/* <AuthContext/> */}
-      
-    </div> 
+
+
+     <>
+     {!user ? <Login handleLogin = {handleLogin}/>: ''}
+     {user == 'admin' ? <AdminDashboard/> : <EmployeeDashboard/>}
+    
+    </>
+   
   )
 }
 
